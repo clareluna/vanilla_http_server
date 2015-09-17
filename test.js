c4c85281a13cfb34b6f07d2426b2b445a87d2e25
clareluna <clarelunamonahan@gmail.com>
2015-09-16 17:07:46 -0700

    working

--- test.js
+++ test.js
@@ -2,17 +2,19 @@
 
 var chai = require('chai');
 var expect = require('chai').expect;
-var vanilla = require('./vanillaHTTPServer/vanillaHTTPserver');
+require('./vanillaHTTPServer/server');
 var http = require('http');
 var chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
 
 describe('vanilla HTTP server should give time or greet person by name', function(){
 	it('should greet someone by name when GET/greet', function(done){
-		chai.request('localhost:3000/greet')
+		chai.request('localhost:4000/greet')
 			.get('/dexter')
 			.end(function(err, res){
 				if(err) {return err};
-				expect(res.header({"Content-type": "text/plain"})).to.eql('hello dexter');
+				expect(res.status).to.eql(200);
+				expect(res.text).to.eql('hello, dexter');
 			done();
 		});	
 	});
